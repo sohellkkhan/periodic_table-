916466d49dfe0ee049625773da034e3dacc32742
Sohel Khan <111686657+sohellkkhan@users.noreply.github.com>
2024-01-12 17:28:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+function H(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="H - Hydrogen 1.008";
+    discription.innerHTML="Hydrogen is the chemical element with the symbol H and atomic number 1.";
+   
+    
+   }
+   function He(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="He - Helium 4.0026";
+    discription.innerHTML="Helium is a chemical element with the symbol He and atomic number 2.";
+
+
+   /* let img=document.createElement('img');
+    img.src="https://images.pexels.com/photos/16045752/pexels-photo-16045752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+      document.body.appendChild(img);
+      img.style.borderRadius="20px";
+      img.style.width="50px";
+      img.style.height="50px";
+      img.style.backgroundRepeat="no-repeat";*/
+
+   }
+
+   function Li(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="Li - Lithium 6.94";
+    discription.innerHTML="Lithium is a chemical element with the symbol Li and atomic number 3";
+   }
+
+   function Be(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="Be - Beryllium 9.0122";
+    discription.innerHTML="Beryllium is a chemical element with the symbol Be and atomic number 4.";
+   }
+
+   function Na(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="Na - Sodium 22.990";
+    discription.innerHTML="Sodium is a chemical element and atomic number 11";
+   }
+
+   function Mg(){
+    let Hydrogen=document.getElementById("c2");
+    let discription=document.getElementById("c6");
+    Hydrogen.innerHTML="Mg - Magnesium 24.305";
+    discription.innerHTML="Magnesium is a chemical element with the symbol Mg and atomic number 12.";
+   }
